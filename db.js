0717b483445f07b17b3d5d0035cb3b200d2cc1ba
NIKHIL TAPARIA <76565229+taparia11@users.noreply.github.com>
2024-04-26 19:45:05 +0530

    DB atlas string

--- db.js
+++ db.js
@@ -1,5 +1,5 @@
 const mongoose = require("mongoose");
-const mongoURI = (`${process.env.MONGODB_KEY}`)
+const mongoURI = "mongodb+srv://taparia:220807@cluster0.fbjet.mongodb.net/shift-planning"
 
 const connectToMongo = ()=>{
     mongoose.connect(mongoURI, ()=>{
